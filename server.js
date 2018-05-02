a2b511374286c5d6e5d7e00e7e0dcc9ce4523924
kannu21 <krishnachittodia@xenolt.com>
2018-05-03 03:01:04 +0530

    Ajax working successful and data of input value is coming just fine

--- server.js
+++ server.js
@@ -14,15 +14,28 @@ var path = require("path");
 
 var app = express();
 
+app.use(express.static(path.join(__dirname, "js")));
+
+
+
 app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname + '/view/MainPage.html'));
 })
 
+app.post('/SearchUrl/', Searching)
+
+function Searching(req, res) {
+    let chk = req.body.SearchUrl;
+    console.log(chk)
+    
+}
 let crawled_web = crawler.crawl_web("http://udacity.github.io/cs101x/index.html");
 var index = crawled_web.index;
 var graph = crawled_web.graph;
 let ranks = Compute_ranks.compute_ranks(graph)
 console.log(ranks)
+
+
 
 
 app.listen(3000, () => console.log("Server is running on: 3000"))
